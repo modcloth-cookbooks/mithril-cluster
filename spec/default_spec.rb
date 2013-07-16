869a789ca823516489e96f220a50bb8332a8a194
Jesse Szwedko <j.szwedko@modcloth.com>
2013-07-16 15:38:22 -0400

    Adding upstart conf file check to spec
    And setting pg_enabled to true.
    
    Signed-off-by: Rafe Colton <r.colton@modcloth.com>

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,6 +6,7 @@ describe 'mithril-cluster::default' do
       log_level: :error
     ) do |node|
       node.normal['mithril_service']['rabbitmq_master'] = 'stub'
+      node.normal['mithril_service']['pg_enabled'] = true
     end.converge described_recipe
   end
 
@@ -35,5 +36,34 @@ describe 'mithril-cluster::default' do
     let(:dir) { dir }
     let(:chef_dir) { chef_run.directory(dir) }
     include_examples 'deploy_directory'
+  end
+
+  it 'creates upstart configuration files' do
+    now = Time.now
+    Time.stub(:now).and_return(now)
+    %w(00 01).each do |num|
+      chef_run.should create_file_with_content "/etc/init/mithril-service-#{num}.conf", <<-EOF.gsub(/^ {6}/, '').chomp
+      # Dropped off by Chef recipe[mithril::cluster] #{now}
+      description "Mithril Service"
+
+      start on filesystem or runlevel [2345]
+      stop on runlevel [!2345]
+
+      setuid mithril
+      umask 022
+
+      respawn
+      respawn limit 3 10
+      chdir /home/mithril
+      script
+        /home/mithril/bin/mithril-server -a=':#{8371 + num.to_i}' \\
+          -amqp.uri='stub' \\
+          -pg \\
+          -pg.uri=$(awk -F: '{ print "postgres://" $4 ":" $5 "@" $1 "/" $3 "?sslmode=disable"}' /home/mithril/.pgpass 2>/dev/null || echo '') \\
+          -d \\
+          -p=''
+      end script
+      EOF
+    end
   end
 end
